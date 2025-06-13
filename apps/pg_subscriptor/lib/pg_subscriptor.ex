afcffd9135bed98e7c2888db6b2adcddffea2fd6
Juraj Paluba <juraj.palubajp@gmail.com>
2025-06-13 17:59:01 +0200

    feat(apps): initialize pg_subscriptor app

---
+++ apps/pg_subscriptor/lib/pg_subscriptor.ex
@@ -0,0 +1,18 @@
+defmodule PgSubscriptor do
+  @moduledoc """
+  Documentation for `PgSubscriptor`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PgSubscriptor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/pg_subscriptor/test/pg_subscriptor_test.exs
@@ -0,0 +1,8 @@
+defmodule PgSubscriptorTest do
+  use ExUnit.Case
+  doctest PgSubscriptor
+
+  test "greets the world" do
+    assert PgSubscriptor.hello() == :world
+  end
+end
