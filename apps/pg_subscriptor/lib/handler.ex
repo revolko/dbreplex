2c9851093d07c5ffa46131b6045cde1216148802
Juraj Paluba <juraj.palubajp@gmail.com>
2025-06-26 22:21:18 +0200

    feat(pg_repl): handle commit, origin, relation messages
    I am not sure how to pattern match on the null-terminated strings.

--- apps/pg_subscriptor/lib/handler.ex
+++ apps/pg_subscriptor/lib/handler.ex
@@ -24,6 +24,18 @@ defmodule PgHandler do
       "B" ->
         handle_beggin(msg)
 
+      "M" ->
+        handle_replication_msg(msg)
+
+      "C" ->
+        handle_commit(msg)
+
+      "O" ->
+        handle_origin(msg)
+
+      "R" ->
+        handle_relation(msg)
+
       _ ->
         Logger.info("Got unknown msg")
     end
@@ -32,5 +44,25 @@ defmodule PgHandler do
   defp handle_beggin(body) do
     Logger.info("Got BEGGIN msg")
     <<_x_log_rec_ptr::64, _timestamp_tz::64, _transaction_id::32>> = body
+  end
+
+  defp handle_replication_msg(_body) do
+    Logger.info("Got PG replication msg")
+  end
+
+  defp handle_commit(body) do
+    Logger.info("Got commit msg")
+    <<_flag::8, _lsn_commit::64, _end_lsn_transaction::64, _timestamp_tz::64>> = body
+  end
+
+  defp handle_origin(body) do
+    Logger.info("Got origin msg")
+    <<_lsn_commit::64, _name>> = body
+  end
+
+  defp handle_relation(body) do
+    Logger.info("Got relation msg")
+    # how to handle strings (null-terminated strings)
+    <<_transaction_id::32, _relation_oid::32, _namespace_and_rest::binary>> = body
   end
 end
