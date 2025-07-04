20ededfb6062c6edf63528206d96fc8c5b1ac7ac
Juraj Paluba <juraj.palubajp@gmail.com>
2025-07-04 16:00:20 +0200

    refactor(pg_subscriber): fix postgres handler name
    Not sure if it is the best approach for tests, but I like it.
    
    ---v2
    Now I am sure about the test name, thanks.
    
    ---v3
    added alias

--- apps/pg_subscriptor/test/handler.exs
+++ apps/pg_subscriptor/test/handler.exs
@@ -1,11 +1,12 @@
-defmodule PgHandlerTest do
+defmodule PgSubscriber.HandlerTest do
   use ExUnit.Case
-  doctest PgHandler
+  alias PgSubscriber.Handler
+  doctest Handler
 
   test "insert message" do
     # TODO: binary representation of columns
     message = <<"I", 1::32, "N", "%{id: 1, name: Juraj}"::binary>>
-    assert GenServer.cast(PgHandler, {:handle, message}) == :ok
+    assert GenServer.cast(PgSubscriber.Handler, {:handle, message}) == :ok
 
     # TODO: the test is used to see logger messages, but the app usually exits before the message is handled
     Process.sleep(100)
