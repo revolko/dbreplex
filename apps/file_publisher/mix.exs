revolko/db_subscriptor

feat(file_target): add simple file target

apps/file_target/mix.exs
@@ -1,0 +1,31 @@
+defmodule FileTarget.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :file_target,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:core, in_umbrella: true}
+    ]
+  end
+end

revolko/dbreplex

apps/file_publisher/mix.exs
@@ -1,0 +1,31 @@
+defmodule FilePublisher.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :file_publisher,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.18",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:core, in_umbrella: true}
+    ]
+  end
+end