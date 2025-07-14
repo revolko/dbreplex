5371e1f21593024f4b367dfdff59a221aeda33c7
Juraj Paluba <juraj.palubajp@gmail.com>
2025-07-14 19:27:26 +0200

    refactor(repo): format repository
    Hopefully the last format commit. ;)

--- apps/pg_subscriber/test/tuple_data_test.exs
+++ apps/pg_subscriber/test/tuple_data_test.exs
@@ -6,16 +6,24 @@ defmodule TupleDataTest do
   doctest TupleData
 
   test "get_tuple_data" do
-    message = <<6::16, "n", "b", 5::32, "Filip", "u", "b", 1::32, "D", "t", 5::32, "Juraj", "n", 55::32, 1::16, "n">>
+    message =
+      <<6::16, "n", "b", 5::32, "Filip", "u", "b", 1::32, "D", "t", 5::32, "Juraj", "n", 55::32,
+        1::16, "n">>
+
     assert {:ok, tuple_data, rest} = TupleData.get_tuple_data(message)
-    assert tuple_data == %TupleData{num_of_cols: 6, columns: [
-      %Column{kind: ?n, value: nil},
-      %Column{kind: ?b, value: "Filip"},
-      %Column{kind: ?u, value: nil},
-      %Column{kind: ?b, value: "D"},
-      %Column{kind: ?t, value: "Juraj"},
-      %Column{kind: ?n, value: nil},
-    ]}
+
+    assert tuple_data == %TupleData{
+             num_of_cols: 6,
+             columns: [
+               %Column{kind: ?n, value: nil},
+               %Column{kind: ?b, value: "Filip"},
+               %Column{kind: ?u, value: nil},
+               %Column{kind: ?b, value: "D"},
+               %Column{kind: ?t, value: "Juraj"},
+               %Column{kind: ?n, value: nil}
+             ]
+           }
+
     assert rest == <<55::32, 1::16, "n">>
   end
 
@@ -45,7 +53,7 @@ defmodule TupleDataTest do
 
     message = <<1::16, "t", 5::16, "Filip">>
     assert {:error, result} = TupleData.get_tuple_data(message)
-    assert result == {:incomplete_column_value, 345705, <<"lip">>}
+    assert result == {:incomplete_column_value, 345_705, <<"lip">>}
   end
 
   test "get_tuple_data_invalid_length" do
