6dbd6d31b68c0331314d8f5f6580531e193470ee
Juraj Paluba <juraj.palubajp@gmail.com>
2025-07-15 00:30:19 +0200

    feat(pg_subscriber): create PG subscriber supervisor
    The idea is that each subscriber will initialize its own Supervisor
    tree. That way we can make sure that the replication client is
    configured with the same handler and that they are intialized in the
    correct order. It will basically make the configuration a bit more
    straighforward.
    
    Note, we need to rething the supervisor child strategy. Currently, if
    you try to configure multiple PG subscribers the second one will not
    initialize because of naming collisions for Repl and Handler servers.
    
    Relates: #20

--- apps/main_app/lib/main_app.ex
+++ apps/main_app/lib/main_app.ex
@@ -3,22 +3,31 @@ defmodule MainApp do
 
   use Application
 
-  @subscriber Application.compile_env(:main_app, :subscriber, nil)
-  @replicator Application.compile_env(:main_app, :replicator, nil)
-  @publisher Application.compile_env(:main_app, :publisher, nil)
+  @subscribers Application.compile_env(:main_app, :subscribers, [])
+  @publishers Application.compile_env(:main_app, :publishers, [])
 
   @impl true
   def start(_type, _args) do
     children = [
-      @subscriber,
-      {@replicator,
-       [host: "localhost", database: "postgres", username: "postgres", password: "postgres"]},
-      {@publisher, "/tmp/replication.log"},
+      get_children_spec(@publishers),
+      get_children_spec(@subscribers)
     ]
+
+    children = List.flatten(children)
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: MainApp.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp get_children_spec(config_properties) do
+    Enum.with_index(config_properties)
+    |> Enum.map(fn {property, index} ->
+      %{
+        id: "#{property.module}#{index}",
+        start: {property.module, :start_link, [property.init_arg]}
+      }
+    end)
   end
 end
