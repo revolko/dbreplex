7161d7dfc9af43ea20a2ba08aa25be97d8f04830
Juraj Paluba <juraj.palubajp@gmail.com>
2025-07-15 22:54:38 +0200

    feat(file_publisher): enable file pub server with generic names
    That way we can start multiple FilePublisher servers at the same time.
    
    ---v2
    Not defining name allows use to create multiple instance of
    FilePublisher without worrying about naming collisions. The messages
    will be send based on the PID of a process.

--- apps/core/lib/core/publisher_contract.ex
+++ apps/core/lib/core/publisher_contract.ex
@@ -6,10 +6,13 @@ defmodule Core.PublisherContract do
   @typedoc "A replication message struct (Insert/Update/Delete)."
   @type message :: struct()
 
+  @typedoc "Name of the publisher server."
+  @type server_pid :: GenServer.name()
+
   @doc """
   Handles an incoming replication message.
 
   Implement this callback in your publisher app.
   """
-  @callback handle_message(message) :: :ok | {:error, term()}
+  @callback handle_message(server_pid, message) :: :ok | {:error, term()}
 end
