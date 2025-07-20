d1faa8baabe2a3c6f3b4a3d4bb33222814214537
Juraj Paluba <juraj.palubajp@gmail.com>
2025-07-20 13:58:21 +0200

    refactor(file_publisher): fix handle_message test

--- apps/file_publisher/test/file_publisher_test.exs
+++ apps/file_publisher/test/file_publisher_test.exs
@@ -4,21 +4,21 @@ defmodule FilePublisherTest do
   doctest FilePublisher
 
   test "receive INSERT message" do
-    # {:ok, _pid} = FilePublisher.start_link("/tmp/replication.log")
-    save_message(5)
+    {:ok, pid} = FilePublisher.start_link("/tmp/replication.log")
+    save_message(pid, 5)
     Process.sleep(100)
   end
 
-  defp save_message(0) do
+  defp save_message(_pid, 0) do
   end
 
-  defp save_message(num) do
+  defp save_message(pid, num) do
     message = %Insert{
       relation_oid: num,
       columns: %{id: 1, name: "Juraj"}
     }
 
-    FilePublisher.handle_message(message)
-    save_message(num - 1)
+    FilePublisher.handle_message(pid, message)
+    save_message(pid, num - 1)
   end
 end
