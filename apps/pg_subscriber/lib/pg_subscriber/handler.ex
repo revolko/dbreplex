36b82bab9d4795eddef898b67f27ad9e70a388ac
Juraj Paluba <juraj.palubajp@gmail.com>
2025-07-21 17:43:57 +0200

    feat(pg_subscriber): handle PG relation message
    I have created Utils module containing generic functions that are used
    throughout different messages.

--- apps/pg_subscriber/lib/pg_subscriber/handler.ex
+++ apps/pg_subscriber/lib/pg_subscriber/handler.ex
@@ -2,11 +2,13 @@ defmodule PgSubscriber.Handler do
   use GenServer
   require Logger
 
+  alias PgSubscriber.Messages.Relation
   alias PgSubscriber.Messages.Delete
   alias Core.Messages.MessageProtocol
   alias PgSubscriber.Messages.Update
   alias Core.Messages.Insert
   alias PgSubscriber.TupleData
+  alias PgSubscriber.Utils
 
   def start_link(default) do
     GenServer.start_link(__MODULE__, default, name: __MODULE__)
@@ -90,30 +92,22 @@ defmodule PgSubscriber.Handler do
   defp handle_origin(body) do
     Logger.info("Got origin msg")
     <<_lsn_commit::64, rest::binary>> = body
-    {name, _} = get_string(rest)
+    {name, _} = Utils.get_string(rest)
     Logger.info("Got postgres origin name #{name}")
   end
 
-  defp handle_relation(body) do
+  defp handle_relation(data) do
     Logger.info("Got relation msg")
-    # how to handle strings (null-terminated strings)
-    <<_relation_oid::32, rest::binary>> = body
-    {namespace, rest} = get_string(rest)
-    {relation, rest} = get_string(rest)
-    <<_repl_identity_setting::8, _columns_num::16, rest::binary>> = rest
-
-    Enum.each(get_columns_info(rest), fn {_flags, col_name, _type_oid, _type_modifier} ->
-      Logger.info(
-        "Got relation namespace: #{namespace}, relation: #{relation}, column: #{col_name}."
-      )
-    end)
+    relation = Relation.from_data!(data)
+    Logger.debug(relation: relation)
+    relation
   end
 
   defp handle_type(body) do
     Logger.info("Got TYPE msg")
     <<_transaction_id::32, _type_oid::32, rest::binary>> = body
-    {_namespace, rest} = get_string(rest)
-    {_type_name, _rest} = get_string(rest)
+    {_namespace, rest} = Utils.get_string(rest)
+    {_type_name, _rest} = Utils.get_string(rest)
   end
 
   defp handle_insert(body) do
@@ -167,28 +161,5 @@ defmodule PgSubscriber.Handler do
     <<relation_oid::32, rest::binary>> = data
     {relation_oids, rest} = get_relations_oids(relations_num - 1, rest)
     {[relation_oid | relation_oids], rest}
-  end
-
-  defp get_columns_info(<<>>) do
-    []
-  end
-
-  defp get_columns_info(<<flags::8, msg::binary>>) do
-    {name, rest} = get_string(msg)
-    <<type_oid::32, type_modifier::32, rest::binary>> = rest
-    [{flags, name, type_oid, type_modifier} | get_columns_info(rest)]
-  end
-
-  defp get_string(<<0>>) do
-    {<<>>, <<>>}
-  end
-
-  defp get_string(<<0, rest::binary>>) do
-    {<<>>, rest}
-  end
-
-  defp get_string(<<char::binary-size(1), rest::binary>>) do
-    {last_char, new_rest} = get_string(rest)
-    {char <> last_char, new_rest}
   end
 end
