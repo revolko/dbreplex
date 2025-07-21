7d9310b192396b042366287dc6a47824671cecd0
Juraj Paluba <juraj.palubajp@gmail.com>
2025-07-21 19:53:58 +0200

    refactor(pg_subscriber): unify behaviour usage
    Added optional `@impl` tag as it makes it more readable and updated the
    type of `from_data!` callback. I am still not 100% happy with the tying,
    I would love for it to be dynamic based on which module implements it,
    but it is good enough for now.

--- apps/pg_subscriber/lib/pg_subscriber/messages/message_behaviour.ex
+++ apps/pg_subscriber/lib/pg_subscriber/messages/message_behaviour.ex
@@ -2,5 +2,5 @@ defmodule PgSubscriber.Messages.MessageBehaviour do
   @doc """
   Converts binary data to the Postgres-specific representation of a message (struct).
   """
-  @callback from_data!(data :: binary()) :: any()
+  @callback from_data!(data :: binary()) :: struct()
 end
