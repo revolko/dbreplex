d04dd993758e5cfb28c1817b36ee5883df7ecd38
Juraj Paluba <juraj.palubajp@gmail.com>
2025-07-29 22:00:57 +0200

    feat(file_publisher): pretify INSERT, UPDATE, DELETE
    OMG I love elixirs pattern matching.
    
    TODO: TRUNCATE.
    
    I am putting this effort to file publisher because I want to support
    basic replication to S3 buckets.

---
+++ apps/file_publisher/lib/file_publisher/serializer.ex
@@ -0,0 +1,34 @@
+defprotocol FilePublisher.Serializer do
+  @moduledoc """
+  Protocol defining serialization for FilePublisher.
+  """
+
+  @doc """
+  Serialize Core message to binary so that it can be written to file.
+  """
+  @spec serialize(message :: t()) :: binary()
+  def serialize(message)
+end
+
+defimpl FilePublisher.Serializer, for: Core.Messages.Insert do
+  def serialize(%Core.Messages.Insert{} = message) do
+    "INSERT INTO #{message.relation_oid} VALUES " <>
+      (Enum.map(message.columns, fn col -> "#{col.value}" end)
+       |> Enum.join(" "))
+  end
+end
+
+defimpl FilePublisher.Serializer, for: Core.Messages.Update do
+  def serialize(%Core.Messages.Update{} = message) do
+    "UPDATE #{message.relation_oid} VALUES " <>
+      (Enum.map(message.columns, fn col -> "#{col.value}" end) |> Enum.join(" ")) <>
+      " WHERE " <> (Enum.map(message.where, fn col -> "#{col.value}" end) |> Enum.join(" "))
+  end
+end
+
+defimpl FilePublisher.Serializer, for: Core.Messages.Delete do
+  def serialize(%Core.Messages.Delete{} = message) do
+    "DELETE #{message.relation_oid} WHERE " <>
+      (Enum.map(message.columns, fn col -> "#{col.value}" end) |> Enum.join(" "))
+  end
+end
