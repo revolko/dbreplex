02a59b8c1ebe01c857e21013e5b42b60e1fdf7bd
Juraj Paluba <juraj.palubajp@gmail.com>
2025-08-04 23:11:25 +0200

    refactor(file_publisher): add column names to where conditions

--- apps/file_publisher/lib/file_publisher/serializer.ex
+++ apps/file_publisher/lib/file_publisher/serializer.ex
@@ -22,13 +22,14 @@ defimpl FilePublisher.Serializer, for: Core.Messages.Update do
   def serialize(%Core.Messages.Update{} = message) do
     "UPDATE #{message.relation_oid} VALUES " <>
       (Enum.map(message.columns, fn col -> "#{col.value}" end) |> Enum.join(" ")) <>
-      " WHERE " <> (Enum.map(message.where, fn col -> "#{col.value}" end) |> Enum.join(" "))
+      " WHERE " <>
+      (Enum.map(message.where, fn col -> "#{col.name}=#{col.value}" end) |> Enum.join(" "))
   end
 end
 
 defimpl FilePublisher.Serializer, for: Core.Messages.Delete do
   def serialize(%Core.Messages.Delete{} = message) do
     "DELETE #{message.relation_oid} WHERE " <>
-      (Enum.map(message.where, fn col -> "#{col.value}" end) |> Enum.join(" "))
+      (Enum.map(message.where, fn col -> "#{col.name}=#{col.value}" end) |> Enum.join(" "))
   end
 end
