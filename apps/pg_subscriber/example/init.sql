9c279cd2b06f2f421ae682bafdeb2a0646f90a79
Juraj Paluba <juraj.palubajp@gmail.com>
2025-08-06 17:20:08 +0200

    feat(pg_subscriber): add table with primary key to example

--- apps/pg_subscriber/example/init.sql
+++ apps/pg_subscriber/example/init.sql
@@ -1,11 +1,19 @@
 CREATE TABLE films (
-    code        integer NOT NULL,
-    title       varchar(255) NOT NULL
+  code integer NOT NULL,
+  title varchar(255) NOT NULL
 );
+ALTER TABLE films REPLICA IDENTITY FULL;
 
 CREATE TABLE distributors (
-     did    integer NOT NULL,
-     name   varchar(255) NOT NULL
+  did integer NOT NULL,
+  name varchar(255) NOT NULL
+);
+ALTER TABLE distributors REPLICA IDENTITY FULL;
+
+CREATE TABLE test_primary (
+  first_name varchar(255) NULL,
+  last_name varchar (255) NOT NULL,
+  id serial PRIMARY KEY
 );
 
 CREATE PUBLICATION postgrex_example FOR ALL TABLES;
