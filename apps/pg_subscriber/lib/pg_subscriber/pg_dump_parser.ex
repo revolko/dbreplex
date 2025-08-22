cc0150cc136387ef39a3357f796953cff4dcb90a
Juraj Paluba <juraj.palubajp@gmail.com>
2025-08-22 20:54:03 +0200

    feat(pg_subscriber): create pg_dump parser
    Just a module with helper functions to parse statements from the
    pg_dump output.

---
+++ apps/pg_subscriber/lib/pg_subscriber/pg_dump_parser.ex
@@ -0,0 +1,67 @@
+defmodule PgSubscriber.PgDumpParser do
+  @moduledoc """
+  Utility module providing functions for parsing pg_dump output.
+  """
+  alias Core.Messages.Insert
+
+  @doc """
+  Filter INSERT statements from the pg_dump output.
+  """
+  def filter_inserts(pg_dump) do
+    filter_statement(pg_dump, "INSERT")
+  end
+
+  @doc """
+  Convert pg_dump INSERT statements to Core Insert messages.
+  """
+  def to_core_insert(pg_dump_inserts) do
+    Enum.map(pg_dump_inserts, fn raw_insert ->
+      # for now the relation oid is the name of the table (should change relation oid to the name)
+      [[relation_oid, columns, values]] =
+        Regex.scan(
+          ~r/INSERT INTO ([^ ]+) \(([^\)]+)\) VALUES \(([^\)]+)\);/,
+          raw_insert,
+          capture: :all_but_first
+        )
+
+      %Insert{
+        relation_oid: relation_oid,
+        columns:
+          Enum.zip_with(String.split(columns, ", "), pg_tuple_to_list(values), fn column_name,
+                                                                                  value ->
+            %{
+              name: column_name,
+              value: value
+            }
+          end),
+        timestamp: ""
+      }
+    end)
+  end
+
+  defp filter_statement(pg_dump, statement_prefix) do
+    lines = String.split(pg_dump, "\n")
+
+    Enum.filter(lines, fn line ->
+      hd(String.split(line, " ")) === statement_prefix
+    end)
+  end
+
+  defp pg_tuple_to_list("") do
+    []
+  end
+
+  defp pg_tuple_to_list(<<char, rest::binary>>) do
+    case char do
+      ?' ->
+        part = "'" <> hd(String.split(rest, "'")) <> "'"
+        {_, rest} = String.split_at(rest, String.length(part) + 1)
+        [part | pg_tuple_to_list(rest)]
+
+      _ ->
+        part = <<char>> <> hd(String.split(rest, ","))
+        {_, rest} = String.split_at(rest, String.length(part) + 1)
+        [part | pg_tuple_to_list(rest)]
+    end
+  end
+end
