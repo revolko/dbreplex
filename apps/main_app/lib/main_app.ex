f0bf457d7a6f23432e925848031546ed276d4804
Juraj Paluba <juraj.palubajp@gmail.com>
2025-09-08 19:45:42 +0200

    refactor(main): use DynamicSupervisor
    Since I want to spawn new publishers/subscribers in runtime, the dynamic
    supervisor must be used.

--- apps/main_app/lib/main_app.ex
+++ apps/main_app/lib/main_app.ex
@@ -3,32 +3,16 @@ defmodule MainApp do
 
   use Application
 
-  @subscribers Application.compile_env(:main_app, :subscribers, [])
-  @publishers Application.compile_env(:main_app, :publishers, [])
-
   @impl true
   def start(_type, _args) do
     children = [
-      {Registry, [keys: :duplicate, name: PublisherRegistry]},
-      get_children_spec(@publishers),
-      get_children_spec(@subscribers)
+      {DynamicSupervisor, strategy: :one_for_one, name: MainApp.DynamicSupervisor},
+      {Registry, [keys: :duplicate, name: PublisherRegistry]}
     ]
-
-    children = List.flatten(children)
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: MainApp.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  defp get_children_spec(config_properties) do
-    Enum.with_index(config_properties)
-    |> Enum.map(fn {property, index} ->
-      %{
-        id: "#{property.module}#{index}",
-        start: {property.module, :start_link, [property.init_arg]}
-      }
-    end)
   end
 end
