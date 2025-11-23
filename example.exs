6836b7c75305148e25dacd5fa02104a18eb4982d
Juraj Paluba <juraj.palubajp@gmail.com>
2025-11-23 14:10:10 +0100

    chore(example): add initialization of Postgres publisher

--- example.exs
+++ example.exs
@@ -11,3 +11,14 @@ DynamicSupervisor.start_child(
   MainApp.DynamicSupervisor,
   {Publishers.File, ["/tmp/replication.log"]}
 )
+
+DynamicSupervisor.start_child(
+  MainApp.DynamicSupervisor,
+  {Publishers.Postgres,
+   connection: [
+     host: "localhost",
+     username: "postgres",
+     database: "postgres",
+     password: "postgres"
+   ]}
+)
